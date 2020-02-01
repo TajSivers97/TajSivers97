c6f3cdd5ede1ee2cae0d4247ffef6f3c118bb690
TajSivers97 <tajsivers@live.com>
2020-02-01 10:49:00 -0500

    Add files via upload

---
+++ FinalProject.js
@@ -0,0 +1,15 @@
+function validateForm(){
+    var textz = document.getElementById("textarea");
+
+    if (textz == ""){
+        alert("Frequently asked question must be filled out!")
+        return false;
+    }
+    else{
+        document.getElementById("submitButton").addEventListener("click",function(){
+
+        }){
+            console.log("Your Frequently asked question has been sent and processed! We will Post the answer ASAP.");
+        }
+    }
+}
